27ca600f1c6cc9b106fd894b9f678076deb1da4e
KeplerInCoding <shreyakv8@gmail.com>
2023-07-31 15:37:04 +0530

    .

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,7 +3,7 @@ import Cards from './Cards';
 
 function Tours ({tours, removeTour}) {
     return(
-        <div className="w-[820px] flex flex-col">
+        <div className="lg:w-[820px] md:w-[700px] w-[400px] flex flex-col">
             <div className="text-center flex self-center justify-center text-4xl w-96 font-extrabold text-pink-800 m-10 mb-7 border-dashed border-4 border-pink-800 rounded-lg p-3">Plan With Love</div>
             <div className="flex flex-wrap justify-center">
                 {
